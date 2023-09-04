c5622e189b279bdf9401f045f6e98568ff9d0741
mepm86 <matper86-github@yahoo.com>
2023-09-04 15:23:16 -0300

    20 upgrade

--- javascript.js
+++
@@ -1,9 +0,0 @@
-
-let lenguaje = "Javascript";
-console.log("Hola matias estos son tus primeros pasos en "+ lenguaje);
-
-let valor1 = 5;
-let valro2 = 10;
-const valor3 = 15;
-let resultado = valor1+valro2+valor3;
-console.log("El resultado de la suma es "+ resultado);
