2d2a30840d19668436ab9eb8cd32232fbd0d52cf
mepm86 <matper86-github@yahoo.com>
2023-09-04 18:51:33 -0300

    34 upgrade

--- prueva/prueva2.js
+++
@@ -1,7 +0,0 @@
-const persona = '{"Nombre":"Marcos","Apellido":"Ferrero","DNI":"226098456","Nacionalidad":"Ecuatoriano","Celular":"0345-569876","Email":"MFerrero23@gmail.com"}';
-const obj = JSON.parse(persona);
-console.log(obj);
-document.getElementById("demo").innerHTML = 
-"Nombre: "+obj.Nombre+
-"Apellido: "+obj.Apellido+
-"DNI :"+obj.DNI;
