3b414a01cc34eac6d8b80e934e5404f8225e226e
mepm86 <matper86-github@yahoo.com>
2023-09-05 16:04:47 -0300

    40 upgrade

--- javascript.js
+++ javascript.js
@@ -3,3 +3,7 @@ if ('serviceWorker' in navigator) {
       .then(reg => console.log('Registro de SW exitoso', reg))
       .catch(err => console.warn('Error al tratar de registrar el sw', err))
   }
+const ocultarElementos = document.getElementsByClassName("seccion");
+  for (let i = 0;i < ocultarElementos.length; i++){
+    ocultarElementos[i].style.display ="none";
+  }
